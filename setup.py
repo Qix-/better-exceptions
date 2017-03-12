750463b3f57b172799ec54811d9d958648862177
Josh Junon <junon@uber.com>
2017-03-12 04:25:47 -0700

    fix repository url in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description = 'Pretty and helpful exceptions, automatically',
     author = 'Josh Junon',
     author_email = 'josh@junon.me',
-    url = 'https://github.com/qix-/better_exceptions',
-    download_url = 'https://github.com/qix-/better_exceptions/archive/0.1.0.tar.gz',
+    url = 'https://github.com/qix-/better-exceptions',
+    download_url = 'https://github.com/qix-/better-exceptions/archive/0.1.0.tar.gz',
     keywords = ['pretty', 'better', 'exceptions', 'exception', 'error', 'local', 'debug', 'debugging', 'locals'],
     classifiers = [],
 )
