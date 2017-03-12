22e08fbb35afb7fe71f4fd6f2d9f4eb15e2d7305
Josh Junon <junon@uber.com>
2017-03-12 04:54:25 -0700

    0.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'better_exceptions',
     packages = ['better_exceptions'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Pretty and helpful exceptions, automatically',
     author = 'Josh Junon',
     author_email = 'josh@junon.me',
     url = 'https://github.com/qix-/better-exceptions',
-    download_url = 'https://github.com/qix-/better-exceptions/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/qix-/better-exceptions/archive/0.1.1.tar.gz',
     keywords = ['pretty', 'better', 'exceptions', 'exception', 'error', 'local', 'debug', 'debugging', 'locals'],
     classifiers = [],
 )
