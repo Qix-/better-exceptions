0cc5dc13d8c1d292c11c1ca5da7e95e4371a6efd
Josh Junon <i.am.qix@gmail.com>
2017-03-22 09:15:33 -0700

    0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'better_exceptions',
     packages = ['better_exceptions'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Pretty and helpful exceptions, automatically',
     author = 'Josh Junon',
     author_email = 'josh@junon.me',
