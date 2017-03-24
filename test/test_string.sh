525012db058ddc6607ad70a8b4a1c1d7609f6a60
Josh Junon <junon@uber.com>
2017-03-23 23:46:57 -0700

    add comment to test_string

--- test/test_string.sh
+++ test/test_string.sh
@@ -3,6 +3,6 @@ set -e
 
 [ -z "${BETEXC_PYTHON}" ] && export BETEXC_PYTHON=python
 
-TEST_STR='import better_exceptions; a = 5; assert a > 10'
+TEST_STR='import better_exceptions; a = 5; assert a > 10 # this should work fine'
 
 "${BETEXC_PYTHON}" -c "${TEST_STR}" these extra arguments should be removed and should not show up 'in' the output
