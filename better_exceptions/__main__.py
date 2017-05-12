a6b49d021f97eb2fb333e0a186cec4427a771664
Josh Junon <josh@junon.me>
2017-05-12 00:09:05 -0700

    run PYTHONSTARTUP in better-exceptions REPL

--- better_exceptions/__main__.py
+++ better_exceptions/__main__.py
@@ -1,9 +1,17 @@
 import argparse
+import imp
+import os
 
 from better_exceptions import interact
 
 parser = argparse.ArgumentParser(description='A Python REPL with better exceptions enabled', prog='python -m better_exceptions')
 parser.add_argument('-q', '--quiet', help="don't show a banner", action='store_true')
+parser.add_argument('-i', '--no-init', dest='no_init', help="don't load ~/.pyinit", action='store_true')
 args = parser.parse_args()
+
+startup_file = os.getenv('PYTHONSTARTUP')
+if not args.no_init and startup_file is not None:
+    with open(startup_file, 'r') as fd:
+        imp.load_module('pystartup', fd, startup_file, ('.py', 'r', imp.PY_SOURCE))
 
 interact(args.quiet)
