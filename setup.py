3d6df183f439144ed3ecde9ddb98ce74fe0e1ad0
Delgan <adrien.gabillaud@gmail.com>
2017-09-05 16:49:55 +0200

    Add __version__ to top level module

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+import re
 from distutils.core import setup
 
-VERSION = '0.1.8'
+with open('better_exceptions/__init__.py', 'r') as file:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        file.read(), re.MULTILINE).group(1)
 
 setup(
     name = 'better_exceptions',
     packages = ['better_exceptions'],
-    version = VERSION,
+    version = version,
     description = 'Pretty and helpful exceptions, automatically',
     author = 'Josh Junon',
     author_email = 'josh@junon.me',
     url = 'https://github.com/qix-/better-exceptions',
-    download_url = 'https://github.com/qix-/better-exceptions/archive/{}.tar.gz'.format(VERSION),
+    download_url = 'https://github.com/qix-/better-exceptions/archive/{}.tar.gz'.format(version),
     keywords = ['pretty', 'better', 'exceptions', 'exception', 'error', 'local', 'debug', 'debugging', 'locals'],
     classifiers = [],
     extras_require = {
