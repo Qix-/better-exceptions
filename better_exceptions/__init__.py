0b135829734ccb12401276defa66d0882be0b93f
Josh Junon <junon@zeit.co>
2018-01-16 16:19:52 +0100

    0.2.1

--- better_exceptions/__init__.py
+++ better_exceptions/__init__.py
@@ -24,7 +24,7 @@ from .log import BetExcLogger, patch as patch_logging
 from .repl import interact, get_repl
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 THEME = THEME.copy()  # Users customizing the theme should not impact core
