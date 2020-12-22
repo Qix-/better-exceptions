f01a790bc8c740a936e678b3ec445758ff0fc904
Sumanth Ratna <sumanthratna@gmail.com>
2020-12-21 19:03:00 -0500

    Create Django middleware

---
+++ better_exceptions/integrations/django.py
@@ -0,0 +1,14 @@
+from sys import exc_info
+from better_exceptions import excepthook
+
+
+class BetterExceptionsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_exception(self, _, exception):
+        _, _, traceback = exc_info()
+        excepthook(exception.__class__, exception, traceback)
