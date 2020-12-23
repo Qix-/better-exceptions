194d95a5f08e56d639b2a7b9d058601b3e994dca
Josh Junon <junon@wavetilt.com>
2020-12-23 02:20:19 +0100

    remove python2 support from tests

--- test_all.sh
+++ test_all.sh
@@ -14,6 +14,10 @@ cd "$(dirname "${0}")"
 PYTHONPATH="$(pwd):$(pwd)/test"
 export PYTHONPATH
 
+if [[ -z "$PYTHON" ]]; then
+	PYTHON=python3
+fi
+
 function normalize {
 	# we translate anything that looks like an address into 0xDEADBEEF
 	# since the addresses change from run to run and break diff testing
@@ -32,47 +36,44 @@ function test_case {
 }
 
 function test_all {
-	test_case "$BETEXC_PYTHON" "test/test.py"
-	test_case "$BETEXC_PYTHON" "test/test_color.py"
-	test_case "$BETEXC_PYTHON" "test/test_encoding.py"
+	test_case "$PYTHON" "test/test.py"
+	test_case "$PYTHON" "test/test_color.py"
+	test_case "$PYTHON" "test/test_encoding.py"
 	test_case "./test/test_interactive.sh"
 	# test_case "./test/test_interactive_raw.sh"
 	test_case "./test/test_string.sh"
-	test_case "$BETEXC_PYTHON" "test/test_logging.py"
-	test_case "$BETEXC_PYTHON" "test/test_truncating.py"
-	test_case "$BETEXC_PYTHON" "test/test_truncating_disabled.py"
-	test_case "$BETEXC_PYTHON" "test/test_indentation_error.py"
-	test_case "$BETEXC_PYTHON" "test/test_syntax_error.py"
-	test_case "$BETEXC_PYTHON" "test/test_unittest_patch.py"
+	test_case "$PYTHON" "test/test_logging.py"
+	test_case "$PYTHON" "test/test_truncating.py"
+	test_case "$PYTHON" "test/test_truncating_disabled.py"
+	test_case "$PYTHON" "test/test_indentation_error.py"
+	test_case "$PYTHON" "test/test_syntax_error.py"
+	test_case "$PYTHON" "test/test_unittest_patch.py"
 
-	if [[ "$BETEXC_PYTHON" == "python3" ]]; then
-		test_case "$BETEXC_PYTHON" "test/test_chaining.py"
+	if [[ "$PYTHON" == "python3" ]]; then
+		test_case "$PYTHON" "test/test_chaining.py"
 	fi
 }
 
 for encoding in ascii "UTF-8"; do
 	for term in xterm vt100 dumb; do
 		for color in 0 1; do
-			for pv in 2 3; do
-				[[ $color == "1" ]] && color_filename="color" || color_filename="nocolor"
-				filename="test/output/python${pv}-${term}-${encoding}-${color_filename}.out"
+			[[ $color == "1" ]] && color_filename="color" || color_filename="nocolor"
+			filename="test/output/python3-${term}-${encoding}-${color_filename}.out"
 
-				export LANG="en_US.${encoding}"
-				export LC_ALL="${LANG}"
-				export PYTHONCOERCECLOCALE=0
-				export PYTHONUTF8=0
-				export TERM="${term}"
-				export FORCE_COLOR="${color}"
-				export BETEXC_PYTHON="python${pv}"
+			export LANG="en_US.${encoding}"
+			export LC_ALL="${LANG}"
+			export PYTHONCOERCECLOCALE=0
+			export PYTHONUTF8=0
+			export TERM="${term}"
+			export FORCE_COLOR="${color}"
 
-				echo -e "\x1b[35;1m${filename}\x1b[m" >&2
-				if $GENERATE; then
-					exec > "$filename"
-					test_all "$filename"
-				else
-					test_all | diff "$(pwd)/${filename}" -
-				fi
-			done
+			echo -e "\x1b[35;1m${filename}\x1b[m" >&2
+			if $GENERATE; then
+				exec > "$filename"
+				test_all "$filename"
+			else
+				test_all | diff "$(pwd)/${filename}" -
+			fi
 		done
 	done
 done
